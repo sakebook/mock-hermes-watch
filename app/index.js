59c05148f3028dcc234674c9f57411a310902b53
Shinya Sakemoto <sakebook@gmail.com>
2018-09-24 19:40:41 +0900

    Add date

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 import clock from 'clock';
 import document from 'document';
 
+const dateText = document.getElementById('date_font');
 const hoursBar = document.getElementById('hours');
 const minutesBar = document.getElementById('minutes');
 
@@ -10,6 +11,7 @@ function onTick() {
   const minutes = now.getMinutes();
   hoursBar.groupTransform.rotate.angle = (hours + minutes / 60) * 30;
   minutesBar.groupTransform.rotate.angle = minutes * 6;
+  dateText.text = now.getDate();
 }
 
 clock.granularity = 'seconds';
